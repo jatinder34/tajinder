f134a11dcb16008962a37ee2b1fad775afc0b9c1
Tajinder Singh <tajindersingh@Tajinders-MacBook-Pro.local>
2019-02-10 01:34:18 +0530

    changes

--- database/migrations/2019_02_09_195834_create_link_filter_table.php
+++ database/migrations/2019_02_09_195834_create_link_filter_table.php
@@ -15,6 +15,9 @@ class CreateLinkFilterTable extends Migration
     {
         Schema::create('link_filter', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('link_id');
+            $table->integet('type')->default(0)->comment('1 for IP,2 for ISP,3 for Browser,4 for OS,5 for Device Type,6 for Countries');
+            $table->string('parameter')->nullable();
             $table->timestamps();
         });
     }
