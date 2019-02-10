a7d7c1ab35864e75c3939ee7a467fc14e2697228
EC2 Default User <ec2-user@ip-172-31-21-161.ap-south-1.compute.internal>
2019-02-10 12:59:03 +0000

    changes

---
+++ database/migrations/2019_02_10_125808_create_isp_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIspTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('isp', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('isp');
+    }
+}
